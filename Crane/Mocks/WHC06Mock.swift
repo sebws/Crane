5584eb6217d8ee847653dbfea1ea74bcede0888f
Sebastian Wesley-Smith <bassyws@gmail.com>
2024-09-05 23:44:27 +1000

    fix: make whc06 mock data more accurate

--- Crane/Mocks/WHC06Mock.swift
+++ Crane/Mocks/WHC06Mock.swift
@@ -17,10 +17,16 @@ private class WHC06CBMPeripheralSpecDelegate: CBMPeripheralSpecDelegate {
                     CBMAdvertisementDataLocalNameKey: "WH-C06",
                     CBMAdvertisementDataServiceUUIDsKey: [],
                     CBMAdvertisementDataIsConnectable: true as NSNumber,
-                    CBMAdvertisementDataManufacturerDataKey: Data([
-                        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                        0x00, 0x00, 0x00, 0x01, UInt8.random(in: 0...255),
-                    ]),
+                    CBMAdvertisementDataManufacturerDataKey: Data(
+                        [
+                            0x00, 0x01, 0x02, 0x03, 0x11, 0x2a, 0xc0, 0x19,
+                            0x11,
+                            0x23, 0xe1, 0x01,
+                        ] + withUnsafeBytes(of: Int16(Double.random(in: 0...20) * 100).bigEndian) { Array($0)} + [
+                            0x01, 0xf4, 0x01, 0xd4,
+                            0xcb,
+                        ]
+                    ),
                 ], interval: 0.25, isAdvertisingWhenConnected: true)
 
             WHC06Mock.simulateAdvertisementChange([config])
@@ -34,8 +40,9 @@ let WHC06Mock = CBMPeripheralSpec.simulatePeripheral().advertising(
         CBMAdvertisementDataServiceUUIDsKey: [],
         CBMAdvertisementDataIsConnectable: true as NSNumber,
         CBMAdvertisementDataManufacturerDataKey: Data([
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x01, 0x0f,
+            0x00, 0x01, 0x02, 0x03, 0x11, 0x2a, 0xc0, 0x19, 0x11, 0x23, 0xe1,
+            0x01, 0x00, 0x00, 0x01, 0xf4, 0x01, 0xd4, 0xcb,
+
         ]),
     ],
     withInterval: 0.1,
